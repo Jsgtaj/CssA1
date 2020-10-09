31ebd91c5ae9c1ce2e1b77267da800aa9d94dc03
Jsgtaj <jasaj316@gmail.com>
2020-10-09 14:20:36 -0400

    added credit to author in footer

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const header = document.querySelector("header");
 let lastPoint = null;
+const navLinks = header.querySelectorAll("#locNav > li > a");
 
 function toggHead() {
   const currentPoint = window.pageYOffset;
   lastPoint < currentPoint ? header.classList.add("hidden") : header.classList.remove("hidden");
   lastPoint = currentPoint;
 }
+function hideHead() {
+  header.classList.add("hidden");
+}
 window.addEventListener("wheel", toggHead);
 window.addEventListener("touchstart", toggHead);
+navLinks.forEach(nav => nav.addEventListener("click", hideHead));
